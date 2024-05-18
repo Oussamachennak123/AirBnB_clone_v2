571c45351ed66653cbefbe00ccf5fe1691b15113
Oussama <chennak.oussama@gmail.com>
2024-05-18 21:08:26 +0100

    create page html with python

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" application flask """
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def is_cool(text='is cool'):
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_int(n):
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<n:int>', strict_slashes=False)
+def H1(n):
+    return render_template('templates/5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
