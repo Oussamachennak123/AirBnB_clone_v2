d9c700373af370b4a880a52422cb94bc6dc22171
Oussama <chennak.oussama@gmail.com>
2024-05-18 20:18:28 +0100

    add function c remplace _ by " "

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
@@ -20,4 +20,4 @@ def c(text):
     return "C" + text.replace("_", "" "")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port="5000")
